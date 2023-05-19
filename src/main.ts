d37bc331a636bc772b3fd7520e99b4d35b9fe597
OlojaMuyiwa <olojam4969@gmail.com>
2023-05-19 21:35:31 +0100

    Add global.css stylesheet and fix typo in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { InternalServerErrorExceptionFilter } from './Exceptions/InternalServerE
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new InternalServerErrorExceptionFilter(), new NotFoundExceptionFilter())
-  app.useStaticAssets(join(__dirname, '..', 'public'))
+  app.useStaticAssets(join(__dirname, '..', 'public',))
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs')
   app.useGlobalPipes(new ValidationPipe({
